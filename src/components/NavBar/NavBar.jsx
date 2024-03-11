b3f1fed5eaacb731a05908ae7e228fbbf1042c4f
Busato Gabriel <busatogabriel96@gmail.com>
2024-03-11 18:57:51 -0300

    asd

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
       {(scrollY<400) ? <div id="NavBarContainerVerti">
       <div id="NavBarContainer">
         <div id="NavBarContainerLeft">
-            <img id="NavBarLogoLeftImg" src="./Assets/LogoBlancoo.png" alt="" />
+            <img id="NavBarLogoLeftImg" src="./Assets/introLogoMainBody.png" alt="" />
         </div>
         <div id="NavBarContainerRight">
           <button className="NavBarContainerRightButton textWhite">Who we are ?</button>
